8bae8276ba77ea0223c1870f7aa5463096511d27
Jooshica <86631084+Jooshica@users.noreply.github.com>
2024-06-18 22:39:45 -0300

    att

--- lib/inventory/inventory.dart
+++ lib/inventory/inventory.dart
@@ -1,18 +1,65 @@
-// inventory.dart
+import 'dart:convert';
+
 import 'item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Inventory {
   List<Item> items;
 
-  Inventory({required this.items});
+  Inventory({required this.items}) {
+    loadInventory();
+  }
 
   // Adiciona um item ao inventário
-  void addItem(Item item) {
+  void addItem(Item item) async {
     items.add(item);
+    item.inInventory = true;
+    await saveInventory();
   }
 
   // Remove um item do inventário
-  void removeItem(Item item) {
+  void removeItem(Item item) async {
     items.remove(item);
+    item.inInventory = false;
+    await saveInventory();
+  }
+
+  // Verifica se o item está no inventário pelo nome
+  bool containsItem(String itemName) {
+    return items.any((item) => item.name == itemName);
+  }
+
+  Future<void> saveInventory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> itemsJson = items.map((item) => itemToJson(item)).toList();
+    await prefs.setStringList('inventory', itemsJson);
+  }
+
+  String itemToJson(Item item) {
+    Map<String, dynamic> json = {
+      'name': item.name,
+      'description': item.description,
+      'imagePath': item.imagePath,
+      'inInventory': item.inInventory,
+    };
+    return jsonEncode(json);
+  }
+
+  Future<void> loadInventory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? itemsJson = prefs.getStringList('inventory');
+    if (itemsJson != null) {
+      items = itemsJson.map((itemJson) => itemFromJson(itemJson)).toList();
+    }
+  }
+
+  Item itemFromJson(String itemJson) {
+    Map<String, dynamic> json = jsonDecode(itemJson);
+    return Item(
+      name: json['name'],
+      description: json['description'],
+      imagePath: json['imagePath'],
+      inInventory: json['inInventory'],
+    );
   }
 }
--- lib/inventory/item.dart
+++ lib/inventory/item.dart
@@ -1,9 +1,36 @@
 class Item {
   final String name;
-  final String imagePath; // Caminho da imagem do item, se aplicável
+  final String description;
+  final String imagePath;
+  bool inInventory; // Propriedade para rastrear se o item está no inventário
+  final bool isVertical; // Indica se o item deve ser exibido na vertical no inventário
+  final double inventoryImageSize; // Tamanho da imagem do item no inventário
 
   Item({
     required this.name,
-    this.imagePath = '', // Por padrão, não há imagem
+    required this.description,
+    required this.imagePath,
+    this.inInventory = false, // Valor padrão para inInventory é false
+    this.isVertical = false, // Valor padrão para isVertical é false
+    this.inventoryImageSize = 50, // Valor padrão para inventoryImageSize é 50
   });
+
+  void setInInventory(bool value) {
+    inInventory = value;
+  }
+
+  @override
+  String toString() {
+    return '$name,$description,$imagePath,$inInventory';
+  }
+
+  static Item fromString(String itemString) {
+    List<String> parts = itemString.split(',');
+    return Item(
+      name: parts[0],
+      description: parts[1],
+      imagePath: parts[2],
+      inInventory: parts[3].toLowerCase() == 'true',
+    );
+  }
 }
